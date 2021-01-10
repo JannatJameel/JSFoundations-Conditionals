b5feebe11673fd9c19856095fb99f18338b0d80b
JannatJameel <j_annat@outlook.com>
2021-01-10 15:20:27 +0300

    Solved the conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
 // Define the numbers and the operation here
 
+const numOne = 10;
+const numTwo = 20;
+const operation = "+";
+let total = 0;
+
 // Write if-statements here
+
+if (operation === "+"){
+    total = numOne + numTwo;
+} else if (operation === "-"){
+    total = numOne - numTwo;
+} else if (operation === "*"){
+    total = numOne * numTwo;
+} else {
+    total = numOne / numTwo;
+}
+
+console.log(`${numOne} ${operation} ${numTwo} = ${total}`);
